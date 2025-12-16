70cb00ac84f04927564d46b777ec182b3158ba35
Madhav kumar <krmadhav312@gmail.com>
2025-12-16 14:49:35 +0530

    added Mouse page and updated main

---
+++ project123/src/Pages/Mouse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mouse = () => {
+  return (
+    <div>
+      <h1>Welcome to Mouse Page</h1>
+    </div>
+  )
+}
+
+export default Mouse
--- project123/src/main.jsx
+++ project123/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import Login from "./Pages/Login";
+import Mouse from "./Pages/Mouse";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Login />
+    <Mouse />
   </React.StrictMode>
 );
