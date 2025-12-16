cffed22b358fb5554e6abe1bd6523b81512f0bd7
Madhav kumar <krmadhav312@gmail.com>
2025-12-16 14:40:13 +0530

    added new page file

---
+++ project123/src/Pages/Practice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <div>
+      <h1>Welcome to github field</h1>
+    </div>
+  )
+}
+
+export default Practice
--- project123/src/main.jsx
+++ project123/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import Login from "./Pages/Login";
-
+import Practice from "./Pages/Practice";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Login />
+    <Practice />
   </React.StrictMode>
 );
